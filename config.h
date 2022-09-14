30537b691edf619bbd40760cc1c24bb013e789e1
Filip Ceglik <fceglik@gmail.com>
2022-09-14 12:46:26 +0200

    added battery status and cleaned up directory

--- config.h
+++ config.h
@@ -86,6 +86,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "terminator", NULL };
+static const char *screenshot[] = {"scrot", "/home/gambo/Pictures/%Y-%m-%d-%T-screenshot.png",NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -97,6 +98,7 @@ static Key keys[] = {
 	{ 0,				BrightUp,  spawn,	   SHCMD("xbacklight -inc 10") },
 	{ 0,				BrightDown,spawn,	   SHCMD("xbacklight -dec 10") },
 	{ SUPER,			XK_l,	   spawn,	   SHCMD("dm-tool lock") },
+	{ 0,				XK_Print,  spawn,	   {.v = screenshot} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
